8053b6b8648291563e688da681cbd4fc9cba630a
AIDEN Cloud & Security Consulting <devsecops@aiden-it-consulting.com>
2026-09-25 10:51:16 +0200

    TWN devops bootcamp: update docker and  vars with more functions dockerlogin dockerpush

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,18 +10,26 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage(String imageName) {
-        script.echo "Building the docker image: ${imageName}"
-        script.withCredentials([
-            script.usernamePassword(
-                credentialsId: 'docker-hub-repo-asambataiden',
-                passwordVariable: 'DOCKERHUB_PASSWORD',
-                usernameVariable: 'DOCKERHUB_USERNAME'
-            )
-        ]) {
+            script.echo "Building the docker image: ${imageName}"
             script.sh "docker build -t ${imageName} ."
+
+    }
+
+    def dockerLogin() {
+        script.withCredentials([
+                script.usernamePassword(
+                        credentialsId: 'docker-hub-repo-asambataiden',
+                        passwordVariable: 'DOCKERHUB_PASSWORD',
+                        usernameVariable: 'DOCKERHUB_USERNAME'
+                )
+        ]) {
             script.sh "echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin"
-            script.sh "docker push ${imageName}"
         }
+    }
+
+
+    def dockerPush(String imageName) {
+        script.sh "docker push ${imageName}"
     }
 }
 
